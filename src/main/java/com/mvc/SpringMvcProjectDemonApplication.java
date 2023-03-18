cb47dd1686b02d26e66532edcc602fdc1da24ebc
Tomasz Kocyła <kocyla.t@gmail.com>
2023-03-18 11:32:28 +0100

    initial commit

---
+++ src/main/java/com/mvc/SpringMvcProjectDemonApplication.java
@@ -0,0 +1,13 @@
+package com.mvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcProjectDemonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvcProjectDemonApplication.class, args);
+	}
+
+}
