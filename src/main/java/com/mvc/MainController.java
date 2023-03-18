23b0ccaee702e5890a4220c736ceee36ec725ba5
Tomasz Kocyła <kocyla.t@gmail.com>
2023-03-18 12:14:42 +0100

    add css and js to our application

--- src/main/java/com/mvc/MainController.java
+++ src/main/java/com/mvc/MainController.java
@@ -4,11 +4,13 @@ package com.mvc;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class MainController {
     @RequestMapping(path = "/imperator", method = RequestMethod.GET)
-    public void getImperatorpage(){
-        System.out.println("Odpalamy stronkę z zarządzaniem imperium");
+    public ModelAndView getImperatorpage(){
+        ModelAndView modelAndView = new ModelAndView("imperator-page.html");
+        return modelAndView;
     }
 }
